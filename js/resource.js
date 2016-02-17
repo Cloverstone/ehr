92a258df5ba30ca44d70c9fdd39b025dd6a327ed
Adam Smallcomb <atsmallcomb@gmail.com>
2016-02-17 13:03:20 -0500

    Updated qrcode

--- js/resource.js
+++ js/resource.js
@@ -1,28 +1,59 @@
 pages = {
 	medical_admin_record: function() {
 		Berry.register({
-			type: 'file',
+			type: 'qrcode',
+			create: function() {
+				return Berry.render('berry_qrcode', this);
+			},
 			setup: function() {
 				this.$el = this.self.find('input');
 				this.$el.off();
 				if(this.onchange !== undefined) {
 					this.$el.on('change', this.onchange);
 				}
-				this.$el.on('change', $.proxy(function(){this.trigger('change');},this));
-			}
+				this.$el.on('change', $.proxy( function() { this.trigger('change'); }, this ));
+			},
+			setValue: function(value) {
+				if(typeof this.lastSaved === 'undefined'){
+					this.lastSaved = value;
+				}
+					this.success = false;
+				if(value.toLowerCase() == this.help.toLowerCase()){
+
+					this.value = value;
+					this.success = true;
+					this.render();
+				}
+			},		
+			satisfied: function(){
+				return this.success
+			},
+			// update: function(silent){
+			// 	if(typeof item === 'object') {
+			// 		$.extend(this.item, item);
+			// 	}
+			// 	$.extend(this, this.item);
+			// 	this.setValue(this.value);
+			// 	this.render();
+			// 	this.setup();
+			// 	if( !silent ) {
+			// 		this.trigger('change');
+			// 	}
+			// }
 		});
+
 		$('[data-order-text]').on('click', function(e){
 			$().berry({legend: 'Confirmation', fields: [
-				{label: 'Patient', type: 'file', name:'patient', required: true, help: e.currentTarget.dataset.name},
-				{label: 'Prescription', type: 'file', name:'prescription', required: true,help: e.currentTarget.dataset.orderText},
-				{label: 'Confirm', value:getNodeIndex(e.currentTarget.parentNode) , type: 'hidden', required: true}
+				{label: 'Patient', type: 'qrcode', name:'patient', required: true, help: e.currentTarget.dataset.name},
+				{label: 'Prescription', type: 'qrcode', name:'prescription', required: true,help: e.currentTarget.dataset.orderText},
+				{label: 'Confirm', value: getNodeIndex(e.currentTarget.parentNode), type: 'hidden', required: true}
 			]}).on('save', function() {
-				if(this.validate()) {
+				if( this.validate() ) {
 					session.medication_administration_record = session.medication_administration_record || {scheduled:[]};
 					var position = parseInt(this.toJSON().confirm, 10);
 					var length = session.medication_administration_record.scheduled.length;
-					if(position > length){
-						for(var i=length;i<position;i++)					
+					if(position > length) {
+						for(var i = length; i < position; i++)					
 							session.medication_administration_record.scheduled[i] = {has_been_administered: false};
 					}
 					session.medication_administration_record.scheduled[position-1] = {has_been_administered: true};
@@ -31,16 +62,16 @@ pages = {
 				}
 			})
 			.on('saved', function(){load(true)})
-			// .on('change:patient', function(item){
-			// 	qrcode.callback = function(data){
-			// 		console.log(data)
-			// 	}
-			// 	processFile.call(this.findByID(item.id).$el[0]);
-			// })
-			.on('change', function(item){
-				qrcode.callback = function(data){
-					$('.hospital-banner').html(data)
-					}
+			.on('change:patient', function(item){
+				qrcode.callback = $.proxy(function(data){
+					this.fields.patient.setValue(data)
+				},this)
+				processFile.call(this.findByID(item.id).$el[0]);
+			})
+			.on('change:prescription', function(item){
+				qrcode.callback = $.proxy(function(data){
+					this.fields.prescription.setValue(data)
+				},this)
 				processFile.call(this.findByID(item.id).$el[0]);
 			})
 		})
